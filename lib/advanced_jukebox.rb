a51313059b8461bea1b89396709545fff59e616d
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-12 05:33:01 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,8 +12,23 @@
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
+ my_songs = {
+ "Go Go GO" => '../audio/Emerald-Park/01.mp3',
+ "LiberTeens" => '../audio/Emerald-Park/02.mp3',
+ "Hamburg" =>  '../audio/Emerald-Park/03.mp3',
+ "Guiding Light" => '../audio/Emerald-Park/04.mp3',
+ "Wolf" => '../audio/Emerald-Park/05.mp3',
+ "Blue" => '../audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => '../audio/Emerald-Park/07.mp3'
+ }
+
 def help
   #this method should be the same as in jukebox.rb
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 
 end
 
@@ -22,6 +37,9 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.each do |title, location|
+    puts "#{title}"
+  end
 end
 
 
@@ -34,12 +52,48 @@ def play(my_songs)
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
   
+  valid_commands = my_songs.keys
+
+  puts "Please enter a song name:"
+  choice = gets.chomp
+  
+  if valid_commands.include?(choice)
+    puts "Playing <#{choice}>"
+    value = my_songs.fetch(choice)
+    system "open #{value}"
+  else
+    puts "Invalid input, please try again."
+  end
+  
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  
+  help
+  loop do 
+    puts "Please enter a command:"
+    value = gets.chomp
+    
+    case value
+      when "help"
+       help
+      when "list"
+       list(my_songs)
+      when "play"
+       play(my_songs)
+      when "exit"
+       exit_jukebox
+       break
+      else
+       puts "Invalid command"
+       puts "===================="
+     end
+    
+  end #loop do 
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,74 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+
+  valid_commands = []
+  length = songs.length
+  max_num = length
+  numbers = *(1..max_num)
+  numbers.map!(&:to_s)
+
+  valid_commands = songs + numbers
+  
+  puts "Please enter a song name or number:"
+  choice = gets.chomp
+  
+  if valid_commands.include?(choice)
+    puts "Command is valid"
+    if choice.length == 1
+      choice = choice.to_i
+      selection = songs[choice-1]
+    else
+      selection = choice
+    end
+    puts "Playing <#{selection}>"
+  else
+    puts "Invalid input, please try again."
+  end
+
+end #play
+
+def list(songs) 
+  songs.each_with_index do |title, index|
+  puts "#{index+1}. #{title}"
+  end
+end #list
+
+def exit_jukebox
+  puts "Goodbye"
+end #exit_jukebox
+
+def run(songs) 
+  #Show user available commands by calling help method
+  help
+  
+  loop do 
+    puts "Please enter a command:"
+    value = gets.chomp
+    
+    case value
+      when "help"
+       help
+      when "list"
+       list(songs)
+      when "play"
+       play(songs)
+      when "exit"
+       exit_jukebox
+       break
+      else
+       puts "Invalid command"
+       puts "===================="
+     end
+    
+  end #loop do
+end # run
