37153b84a305d4f0c70f3e86454345346c9e07a1
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-12 04:57:06 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -38,7 +38,7 @@ def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
   my_songs.each do |title, location|
-    puts "title"
+    puts "#{title}"
   end
 end
 
