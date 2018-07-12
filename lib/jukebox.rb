f0031169da07d43317501cdf4d2f9c8dc5c86d4c
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-12 02:21:46 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,9 +11,23 @@ songs = [
 ]
 
 def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
-def play 
+def play(songs)
+  puts "1. Phoenix - 1901"
+  puts "2. Tokyo Police Club - Wait Up"
+  puts "3. Sufjan Stevens - Too Much"
+  puts "4. The Naked and the Famous - Young Blood"
+  puts "5. (Far From) Home - Tiga"
+  puts "6. The Cults - Abducted"
+  puts "7. Phoenix - Consolation Prizes"
+  puts "8. Harry Chapin - Cats in the Cradle"
+  puts "9. Amos Lee - Keep It Loose, Keept It Tight"
 end
 
 def list 
